6b699a70bf3f885133fbf67ef1b30f12bb2122cd
letyh2 <hatena898910@gmail.com>
2017-11-02 16:12:34 -0600

    cambios validar tarjeta

--- cifrado-cesar/js/app.js
+++ cifrado-cesar/js/app.js
@@ -1,5 +1,1 @@
-var testString = "abcde";
-testString.charAt(2);
-console.log(testString);
-var codigo = testString.CharCodeAt(2);
-console.log(codigo);
+alert("cifrado cesar");
--- validar-tarjeta/js/app.js
+++ validar-tarjeta/js/app.js
@@ -1,12 +1,23 @@
 alert("validar tarjeta");
 
-var validateCard = [1,2,3,4,5,6,7,8,9];
-for(i=0;i <= validateCard.length;i++){
 
+console.log(reverse(validarTarjeta));
+var tarjeta=parseInt(prompt("¿cual es el numero de tarjeta?"));
+if(tarjeta%2==0){
+  alert(true);
+}else{
+  alert(false);
+}
 
-
-
-
-
+var reverse =function(array){
+for(i=0;i<=tarjeta.length;i++){
+  var newArray= array.pop();
+  array.splice(i,0,newArray);
 
 }
+return array;
+
+}
+
+console.log(reverse(tarjeta));
+console.log(newArray);
